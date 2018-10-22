8967dffdeec6654b76d9ca00a84fec1447d25693
sasampl <apilsin46@gmail.com>
2018-10-22 19:57:27 +0600

    1398

--- mahc.py
+++ mahc.py
@@ -57,34 +57,64 @@
 #if (x/4) or (x!/100):
 #else:
 
-from math import sqrt
-a = float(input("Введите значение a: "))
-print ("Введите значение b")
-b = int(input())
-print ("Введите значение c")
-c = int(input())
-d=(b*b)-(4*a*c)
-if a==0:
-    if b == 0:
-        if c == 0:
-            print("Любое решение")
-    else:
-        print("Корней нет")
-    x=-c/b
-    print("Значение x = {}". format(x))
-elif d>0:
-    x1=(-b+sqrt(d))/(a*2)
-    x2=(-b-sqrt(d))/(a*2)
-    print("Значение x1 = {}". format(x1))
-    print("Значение x2 = {}". format(x2))
-elif d==0:
-    x=(-b/(a*2))
-    print("Значение x = {}". format(x))
-elif d<0:
-    print("Рещений нет")
 
 
+# from math import sqrt
+# a = float(input("Введите значение a: "))
+# print ("Введите значение b")
+# b = int(input())
+# print ("Введите значение c")
+# c = int(input())
+# d=(b*b)-(4*a*c)
+# if a==0:
+#     if b == 0:
+#         if c == 0:
+#             print("Любое решение")
+#     else:
+#         print("Корней нет")
+#     x=-c/b
+#     print("Значение x = {}". format(x))
+# elif d>0:
+#     x1=(-b+sqrt(d))/(a*2)
+#     x2=(-b-sqrt(d))/(a*2)
+#     print("Значение x1 = {}". format(x1))
+#     print("Значение x2 = {}". format(x2))
+# elif d==0:
+#     x=(-b/(a*2))
+#     print("Значение x = {}". format(x))
+# elif d<0:
+#     print("Рещений нет")
 
 
+# for i in range(n):
+#     print(i)
+
+# for i in range( 1, 5):
+#     print(i)
 
     
+
+# for i in range( 5, 1, -2):
+#     print(i)
+
+# n = int(input())
+# for i in range( 1, n+1 ):
+#     print(i, end="")
+#     if n != i:
+#         print(" ", end="")
+
+
+# n = int(input())
+# for i in range( 1, n+1, 1):
+#     for j in range( 1, n+1):
+#         print("*", end="")
+#     print()
+
+n = int(input())
+for i in range( n-1, 1, 1):
+    print(i)
+# for j in range( )
+
+
+
+        
