3f8d0c9159875ebf523b7da9bc6a8ab2a6f38bab
sasampl <apilsin46@gmail.com>
2018-10-24 19:37:03 +0600

    mgn

--- mahc.py
+++ mahc.py
@@ -110,11 +110,20 @@
 #         print("*", end="")
 #     print()
 
-n = int(input())
-for i in range( n-1, 1, 1):
-    print(i)
+
 # for j in range( )
 
 
 
-        
+n = int(input())
+for i in range( n, 1, -1):
+    for j in range(i):
+        print("*", end="")
+    print()
+for i in range( 1, n+1, 1):
+    for j in range(i):
+        print("*", end="")
+    print()
+
+
+
